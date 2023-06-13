b2eb4f280821ccccad5620dffdc97da720295132
Mutiat Adepoju <120299296+Rike12@users.noreply.github.com>
2023-06-13 01:49:54 +0100

    git commit-m

--- js/main.js
+++ js/main.js
@@ -138,24 +138,25 @@
   	/* Navigation Menu
    ------------------------------------------------------ */  
    
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger= document.querySelector(".hamburger");
+const menu=document.querySelector(".menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+
 }
 
-const navLink = document.querySelectorAll(".nav-link");
+onClick.addEventListener("click", closeham);
 
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
+function closeham() {
+  menu.classList.toggle("active");
+  hamburger.classList.toggle("active");
 }
+
+
 
 
    /*---------------------------------------------------- */
